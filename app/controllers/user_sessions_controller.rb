85500893499b8d89b06c4a736dd86082a32802fa
saki-44 <saki.tsujimatsu@gmail.com>
2023-11-05 16:34:14 -0800

    フラッシュメッセージの設定

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,15 +4,16 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path
+      redirect_back_or_to root_path, success: t('.success')
     else
-      render :new
+      flash.now[:danger] = t('.fail')
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     logout
-    redirect_to root_path
+    redirect_to root_path, status: :see_other, success: t('.success')
   end
 
 end
