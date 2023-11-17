94e72bbd799744a0ccf1d01be0eac8cc75caf529
saki-44 <saki.tsujimatsu@gmail.com>
2023-11-17 14:50:53 -0800

    creat stub

--- app/controllers/stubs_controller.rb
+++ app/controllers/stubs_controller.rb
@@ -2,4 +2,29 @@ class StubsController < ApplicationController
   def index
     @stubs = Stub.where(user_id: current_user.id).includes(:user).order(created_at: :desc)
   end
+
+  def new
+    @stub = Stub.new
+    @categories = Category.all
+  end
+
+  def create
+    @stub = current_user.stubs.build(stub_params)
+    if @stub.save
+      redirect_to stubs_path, success: t('defaults.message.created', item: Stub.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.message.not_created', item: Stub.model_name.human)
+      render :new
+    end
+  end
+
+  def show
+    @stub = Stub.find(params[:id])
+  end
+
+  private
+
+  def stub_params
+    params.require(:stub).permit(:title, :address, :memo, :public, :category_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   delete 'logout', to: 'user_sessions#destroy'
 
   resources :users, only: %i[new create]
-  resources :stubs, only: %i[index]
+  resources :stubs, only: %i[index new create]
 end
