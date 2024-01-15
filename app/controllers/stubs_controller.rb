d155e06461cbe935776309e7ca66a9fad75fbf57
saki-44 <saki.tsujimatsu@gmail.com>
2024-01-15 15:47:12 -0800

    stub編集、削除機能実装

--- app/controllers/stubs_controller.rb
+++ app/controllers/stubs_controller.rb
@@ -1,4 +1,6 @@
 class StubsController < ApplicationController
+  before_action :find_stub, only: [:edit, :update, :destroy]
+
   def index
     @stubs = Stub.where(user_id: current_user.id).includes(:user).order(created_at: :desc)
   end
@@ -23,9 +25,32 @@ class StubsController < ApplicationController
     @stub = Stub.find(params[:id])
   end
 
+  def edit
+    @categories = Category.all
+  end
+
+  def update
+    if @stub.update(stub_params)
+      redirect_to @stub, success: t('defaults.message.updated', item: Stub.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.message.not_updated', item: Stub.model_name.human)
+      render :edit
+    end
+  end
+
+  def destroy
+    @stub.destroy!
+    redirect_to stubs_path, status: :see_other, success: t('defaults.message.deleted', item: Stub.model_name.human)
+  end
+
+
   private
 
   def stub_params
-    params.require(:stub).permit(:title, :address, :memo, :public, :category_id, :stub_image, :stub_image_cache)
+    params.require(:stub).permit(:title, :address, :memo, :public, :category_id, :stub_image, :stub_image_cache, :date)
+  end
+
+  def find_stub
+    @stub = current_user.stubs.find(params[:id])
   end
 end
